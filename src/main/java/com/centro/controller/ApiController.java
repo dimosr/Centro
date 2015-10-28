b7a7128b089924545b29eb8fb3424917770b6635
Raptis Dimos <raptis.dimos@yahoo.gr>
2015-10-28 00:50:55 +0000

    developer controller for API requests

---
+++ src/main/java/com/centro/controller/ApiController.java
@@ -0,0 +1,44 @@
+package com.centro.controller;
+
+import com.centro.util.GeoCoordinate;
+import com.centro.util.algo.CentralPointCalculator;
+import com.centro.util.algo.MiddlePointCalculator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ApiController {
+    
+    private static final String apiVersion = "1.0";
+    
+    @Autowired
+    @Qualifier("middlePointCalculator")
+    private CentralPointCalculator calculator;
+    
+    @RequestMapping(value = "/api", method = RequestMethod.GET, produces="application/json")
+    public @ResponseBody String index() {
+        return new String("{ apiVersion : " + apiVersion + "}");
+    }
+    
+    @RequestMapping(value = "/api/central", method = RequestMethod.POST, produces="application/json", consumes="application/json")
+    public @ResponseBody String central(@RequestBody String input) throws JsonProcessingException, IOException {
+        ObjectMapper jsonMapper = new ObjectMapper();
+        
+        List<GeoCoordinate> locations = jsonMapper.readValue(input, new TypeReference<List<GeoCoordinate>>(){});
+        
+        GeoCoordinate centralPoint = calculator.getCentralPoint(locations);
+        String output = jsonMapper.writeValueAsString(centralPoint);
+        return output;
+    }
+}
