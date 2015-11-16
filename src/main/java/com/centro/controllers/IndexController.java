702cd5a509ac20d8126ab3774ebdc557bdb99ca7
Raptis Dimos <raptis.dimos@yahoo.gr>
2015-11-16 21:32:22 +0000

    removed dummy code

--- src/main/java/com/centro/controllers/IndexController.java
+++ src/main/java/com/centro/controllers/IndexController.java
@@ -1,37 +1,16 @@
 package com.centro.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.centro.model.SignupForm;
 
 @Controller
 public class IndexController {
 	 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public ModelAndView index() {
-        return new ModelAndView("index", "signupForm", new SignupForm());
+    public String index() {
+        return "index";
     }
  
-    @RequestMapping(value = "/create", method = RequestMethod.POST)
-    public String create(Model model, SignupForm signupForm, BindingResult result, RedirectAttributes redirectAttributes) {
- 
-        if (result.hasErrors()) {
-            return "index";
-        }
- 
-        return "show";
-     
-    }
- 
-    @RequestMapping(value = "/security-error", method = RequestMethod.GET)
-    public String securityError(RedirectAttributes redirectAttributes) {
-        redirectAttributes.addFlashAttribute("page_error", "You do have have permission to do that!");
-        return "redirect:/";
-    }
 }
--- src/main/java/com/centro/model/SignupForm.java
+++
@@ -1,5 +0,0 @@
-package com.centro.model;
-
-public class SignupForm {
-
-}
